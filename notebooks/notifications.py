f144c39c43ed856fb14f929c597307af1d1a6dec
Olga Botvinnik <olga.botvinnik@gmail.com>
2024-10-31 23:33:47 +0000

    Update format of logger

--- notebooks/notifications.py
+++ notebooks/notifications.py
@@ -4,15 +4,20 @@ import logging
 # logger = logging.getLogger(__name__)
 
 logging.basicConfig(
-    format="{asctime} - {levelname}",
+    format="{asctime} - {levelname} - {message}",
     style="{",
     datefmt="%Y-%m-%d %H:%M",
 )
 
+logger = logging.getLogger()
+# Write to stderr
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+
 
 def notify(msg):
-    logging.debug(f"\n{msg}")
+    logger.debug(msg)
 
 
 def notify_done():
-    logging.debug("\tDone.")
+    logger.debug("Done.")
